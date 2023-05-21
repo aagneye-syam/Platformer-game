3ede18f4b0b142f26caacfff25e4be972cd64a77
aagneye-syam <saagneye2003@gmail.com>
2023-05-21 15:17:23 +0530

    Background image added

--- platformer.py
+++ platformer.py
@@ -8,16 +8,39 @@ from os.path import isfile, join
 pygame.init()
 pygame.display.set_caption("Platformer Game")
 
-BG_COLOR = (255, 255, 255)
 WIDTH,HEIGHT = 1000, 800
 FPS = 60
 PLAYER_VEL = 5
 
 window = pygame.display.set_mode((WIDTH,HEIGHT))
 
+
+def get_background(name):
+    image = pygame.image.load(join("assets","Background",name))
+    _, _, width,height = image.get_rect()
+    tiles = []
+
+    for i in range(WIDTH//width+1):
+        for j in range(HEIGHT//height+1):
+            pos = [i*width, j*height]
+            tiles.append(pos)
+
+    return tiles, image
+
+def draw(window, background, bg_image):
+
+    for tile in background:
+        window.blit(bg_image,tile)
+
+
+
+    pygame.display.update()
+
+
 def main(window):
     
     clock = pygame.time.Clock()
+    background, bg_image = get_background("Gray.png")
 
     run = True
     while run :
@@ -27,6 +50,10 @@ def main(window):
             if event.type == pygame.QUIT:
                  run = False
                  break
+            
+        draw(window,background, bg_image)
+
+
     pygame.quit()
     quit()
 
