b970ad426dcc0511e958fa18aff4948dc41ceeab
aagneye-syam <saagneye2003@gmail.com>
2023-05-22 09:54:10 +0530

    Player movement enabled

--- platformer.py
+++ platformer.py
@@ -77,6 +77,18 @@ def draw(window, background, bg_image,player):
     pygame.display.update()
 
 
+def handle_move(player):
+    keys = pygame.key.get_pressed()
+
+    player.x_vel = 0
+    if keys[pygame.K_LEFT]:
+        player.move_left(PLAYER_VEL)
+
+    if keys[pygame.K_RIGHT]:
+        player.move_right(PLAYER_VEL)
+
+
+
 def main(window):
     
     clock = pygame.time.Clock()
@@ -92,7 +104,10 @@ def main(window):
             if event.type == pygame.QUIT:
                  run = False
                  break
-            
+
+
+        player.loop(FPS)    
+        handle_move(player)    
         draw(window,background, bg_image,player)
 
 
