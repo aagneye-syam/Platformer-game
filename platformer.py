f8708a569a9e873f32e6eeb1f031af51883e4d37
aagneye-syam <saagneye2003@gmail.com>
2023-05-23 23:14:58 +0530

    Collision with terrains added

--- platformer.py
+++ platformer.py
@@ -89,11 +89,20 @@ class Player(pygame.sprite.Sprite):
             self.animation_count = 0
 
     def loop(self, fps):
-        #self.y_vel += min(1, (self.fall_count / fps) * self.GRAVITY)
+        self.y_vel += min(1, (self.fall_count / fps) * self.GRAVITY)
         self.move(self.x_vel, self.y_vel)
 
         self.fall_count += 1
         self.update_sprite()
+
+    def landed(self):
+        self.fall_count = 0
+        self.y_vel = 0
+        self.jump_count = 0
+
+    def hit_head(self):
+        self_count = 0
+        self.y_vel *= -1        
 
     def update_sprite(self):
         sprite_sheet = "idle"
@@ -166,7 +175,24 @@ def draw(window, background, bg_image, player,objects):
     pygame.display.update()
 
 
-def handle_move(player):
+def handle_vertical_collision(player, objects, dy):
+    collided_objects = []
+    for obj in objects:
+        if pygame.sprite.collide_mask(player,obj):
+            if dy > 0:
+                player.rect.bottom = obj.rect.top
+                player.landed()
+            elif dy < 0:
+                player.rect.top = obj.rect.bottom
+                player.hit_head()
+
+        collided_objects.append(obj)
+
+    return collided_objects            
+
+
+
+def handle_move(player, objects):
     keys = pygame.key.get_pressed()
 
     player.x_vel = 0
@@ -175,6 +201,9 @@ def handle_move(player):
 
     if keys[pygame.K_RIGHT]:
         player.move_right(PLAYER_VEL)
+
+
+    handle_vertical_collision(player, objects, player.y_vel)   
 
 
 def main(window):
@@ -198,7 +227,7 @@ def main(window):
                 break
 
         player.loop(FPS)
-        handle_move(player)
+        handle_move(player,floor)
         draw(window, background, bg_image, player, floor)
 
     pygame.quit()
